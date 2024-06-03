a1d521385b94ab9fb2010e2df979ceefe37430f7
taesung.kim (Stussy) <kts6056@gmail.com>
2024-06-03 14:43:20 +0900

    ktlint 적용

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
--- feature/b/feature4/src/test/java/com/example/feature4/ExampleUnitTest.kt
+++ feature/b/feature4/src/test/java/com/example/feature4/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.feature4
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
